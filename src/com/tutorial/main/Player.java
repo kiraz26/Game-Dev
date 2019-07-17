a242c2b8f595432999a7e8eee5fe99533e01952e
kiraz26 <halilkirazz@gmail.com>
2019-07-17 12:53:12 -0400

    Game Dev 1 by kiraz

---
+++ src/com/tutorial/main/Player.java
@@ -0,0 +1,36 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Color.*;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class Player extends GameObject {
+
+	Random r = new Random();
+
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+
+	}
+
+	public void tick() {
+		x += velX;
+		y += velY;
+		
+		x= Game.clamp(x, 0, Game.WIDTH-37);
+		y= Game.clamp(y, 0, Game.HEIGHT-60);
+
+	}
+
+	public void render(Graphics g) {
+
+		g.setColor(Color.white);
+
+		g.fillRect(x, y, 32, 32);
+
+	}
+	
+	 
+
+}
