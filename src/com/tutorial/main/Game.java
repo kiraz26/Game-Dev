923f57a899c65b0cf2b24b3668865c7df8141edb
Halil Kiraz <halilkiraz@Halils-MacBook-Air.local>
2020-11-27 18:16:08 -0700

    Screen size updated

--- src/com/tutorial/main/Game.java
+++ src/com/tutorial/main/Game.java
@@ -10,7 +10,7 @@ public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1550691097823471818L;
 
-	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	public static final int WIDTH = 1280, HEIGHT = WIDTH / 12 * 9;
 	private Thread thread;
 	private boolean running = false;
 
